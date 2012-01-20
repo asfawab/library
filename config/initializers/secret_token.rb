4c3cb2f6abaf4a377a22e34369dc7f44dec22b82
Asfaw abera <workin2shyahoo.compwd>
2012-01-20 16:55:59 +0200

    initial rails 3app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Library::Application.config.secret_token = 'e1b7b4d8fce9b3ca3bfd8440373284affe3063f294f1d419a3262ee02b77e74888beaa00e76259945c1f0380bd5071128b56ffcd97880b32c66ea10a97a6eded'
